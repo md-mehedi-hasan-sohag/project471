b949cc8dc9acd0d307305e33985d92942279cfbf
md-mehedi-hasan-sohag <sohag7808@gmail.com>
2024-03-23 23:57:34 +0600

    my commit

--- skincare_online/urls.py
+++ skincare_online/urls.py
@@ -25,6 +25,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('testing.urls')),
+    path('userlogin/',include('userlogin.urls'))
 
 ]
 
---
+++ userlogin/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'userlogin/index.html')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse('User logged in successfully')
+        if '@gmail.com' in username:
+            user = User.objects.filter(email=username).first()
+            if user is not None:                #tasmi77#tasmi@gmail.com#12345
+                username = user.username
+        if user is not None:
+            login(request, user)
+            return HttpResponse('User logged in successfully')
+        if '@admin.com' in username:
+            user = User.objects.filter(email=username).first()
+            if user is not None:                #sohag@admin.com#asdfg
+                username = user.username
+        if user is not None:
+            login(request, user)
+            return render(request, 'userlogin/adminhome.html')
+        return render(request, 'userlogin/signin.html', {'error': 'Invalid credentials'})
+    return render(request, 'userlogin/signin.html')
+
+def signup(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm-password')
+
+        if User.objects.filter(email=email).exists():
+            return render(request, 'userlogin/signup.html', {'error': 'Email already exists'})
+        
+        if User.objects.filter(username=username).exists():
+            return render(request, 'userlogin/signup.html', {'error': 'Username already exists'})
+
+        if password != confirm_password:
+            return render(request, 'userlogin/signup.html', {'error': 'Passwords do not match'})
+        
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+
+        if new_user is None:
+            return render(request, 'userlogin/signup.html', {'error': 'Error creating user'})
+        
+        new_user.first_name = firstname
+        new_user.last_name = lastname
+        new_user.save()
+
+        return render(request, 'userlogin/signin.html')
+
+    return render(request, 'userlogin/signup.html')
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully!")
+    return redirect('home')
